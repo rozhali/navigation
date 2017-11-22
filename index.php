7b5071738d68f060bb82edd05766fef994d970f1
Rozh Ali <rozh52malak@gmail.com>
2017-11-22 10:30:34 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+	
+	<!-- Basic
+  	================================================== -->
+	<meta charset="utf-8">
+	<title>VHM Ventures</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+
+	<!-- Mobile Specific Metas
+  	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  	================================================== -->
+	<link rel="stylesheet" href="./assets/css/basic.css" type='text/css'>
+	<link rel="stylesheet" href="./assets/css/layout.css" type='text/css'>
+	<link rel="stylesheet" href="./assets/css/responsive.css" type='text/css'>
+	<link rel="stylesheet" href="./assets/css/elements.css" type='text/css'>
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="icon" href="./assets/images/icons/vhm-favicon-w192.png" sizes="32x32">
+	<link rel="icon" href="./assets/images/icons/vhm-favicon-w192.png" sizes="192x192">
+	<link rel="apple-touch-icon-precomposed" href="./assets/images/icons/vhm-favicon-w192.png" sizes="180x180">
+	<meta name="msapplication-TileImage" content="./assets/images/icons/vhm-favicon-w192.png">
+
+</head>
+<body>
+
+	<!-- wrapper -->
+	<div class="wrapper">
+		<?php include "./inc/_header.php" ; ?>
+
+		<!-- Home -->
+		<section id="home" class="section home home-slider">
+			<div class="section-content container full-height full-width">
+				<div class="slide one-full column slide-theme-light">
+					<div class="container full-width full-height">
+						<div class="slide-background one-full column">
+							<div class="background image" data-url="./assets/images/pexels-photo-210540.jpeg"></div>
+						</div>
+						<div class="slide-content one-full column">
+							<div class="container full-height v-center">
+								<div class="headline one-full column">Brilliant Measures</div>
+								<div class="tagline one-full column">Leading Disruptive Investments</div>
+								<div class="cta one-full column">
+									<a href="#about" class="button button-custom"><span>Discover</span></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Home -->
+
+
+
+		<!-- About -->
+		<section id="about" class="section about" id="content">
+			<div class="section-content container full-width">
+				<div class="section-background one-full column">
+					<div class="background image" style="opacity: 0.6;" data-url="./assets/images/space.jpg"></div>		
+				</div>
+				<div class="section-text one-full column">
+					<div class="container full-width full-height v-center">
+						<div class="one-full column">
+							<div class="container narrow">
+								<div class="one-half column">
+									<div class="container full-width">
+										<div class="headline one-full column">VHM</div>
+										<div class="description one-full column with-bottom">VHM Ventures invests in biomedical breakthroughs that change the world of medicine. Our co-founders have all been involved in exactly these types of ventures – from building world-class hospitals through developing new ways of interrogating genomes to changing the way that drugs are delivered. Our companies have generated billions of dollars in value from humble beginnings, and building them was hard work. We loved it. <br/>Our team formed VHM Ventures to mobilize capital into those opportunities when they are about to enter a major inflection point. And now is the time, because innovations in healthcare are exploding, particularly around the convergence of pharmaceuticals, devices and technology. If you share our passion for breakthroughs, join us!</div>
+										<!-- <div class="one-full column with-bottom">
+											<img style="margin-bottom: 8%; width: 25%;" src="https://res.cloudinary.com/vwp/image/upload/c_scale,w_260/v1506288928/rani-logo_wdct5f.png">
+										</div> -->
+										<div class="cta one-full column">
+											<a href="#contact" class="button light"><span>Contact</span></a>
+											<a href="https://ranitherapeutics.com/" rel="external" target="_blank"><img style="padding-left: 15%; width: 35%;" src="https://res.cloudinary.com/vwp/image/upload/c_scale,w_260/v1506288928/rani-logo_wdct5f.png"></a>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- About -->
+
+
+
+		<!-- Team -->
+		<section id="team" class="section blog blog-archive" data-post-current="1" data-post-pagination="infinite" data-post-paged="9">
+			<div class="section-header container full-width">
+				<div class="media one-full column">
+					<div class="background image overlay-none" data-url="./assets/images/rio-de-janeiro-olympics.jpeg"></div>
+				</div>
+				<div class="container full-height h-center v-center">
+					<div class="headline one-full column">Team</div>
+				</div>
+				<!-- <div class="headline one-full column">Team</div> -->
+			</div>
+			<div class="section-content container full-width">
+				<div class="blog-content one-full column">
+					<div class="container full-width v-top narrow">
+						<div class="blog-main column"  style="margin: 0 10%;">
+							<h3 style="text-align: center;">Our backgrounds range across neuroscience, genetics, software and business – and we all share a passion for backing innovations that disrupt large healthcare markets.</h3>
+						</div>
+						<div class="blog-main column">
+							<div class="container full-width">
+								<div class="blog-grid one-full column">
+									<div class="container full-width">
+
+										<!-- post -->
+										<div class="post column active" style="margin-left: 4%;">
+											<div class="container full-width">
+												<div class="image one-full column">
+													<a href="https://www.linkedin.com/in/andrewfarq" rel="external" target="_blank">
+														<img width="640" height="400" src="https://res.cloudinary.com/vwp/image/upload/c_scale,w_250/v1494963282/incube-vc-andrew-f_o7eg4l.png" alt="Andrew Farquharson">
+													</a>
+												</div>
+												<div class="info one-full column">
+													<div class="container full-width">
+														<div class="title one-full column">
+															<a href="https://www.linkedin.com/in/andrewfarq" rel="external" target="_blank">Andrew Farquharson</a>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<!-- post -->
+
+										<!-- post -->
+										<div class="post column active">
+											<div class="container full-width">
+												<div class="image one-full column">
+													<a href="https://www.linkedin.com/in/talat" rel="external" target="_blank">
+														<img width="640" height="400" src="https://res.cloudinary.com/vwp/image/upload/c_scale,w_250/v1495567928/talat2_jdkg7j.png" alt="Talat Imran">
+													</a>
+												</div>
+												<div class="info one-full column">
+													<div class="container full-width">
+														<div class="title one-full column">
+															<a href="https://www.linkedin.com/in/talat" rel="external" target="_blank">Talat Imran</a>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<!-- post -->
+
+										<!-- post -->
+										<div class="post column active">
+											<div class="container full-width">
+												<div class="image one-full column">
+													<a href="https://www.linkedin.com/in/mar-perez-08423228"  rel="external" target="_blank">
+														<img width="640" height="400" src="https://res.cloudinary.com/vwp/image/upload/c_scale,w_250/v1494963280/incube-vc-mar_ihjjso.png" alt="Mar Perez">
+													</a>
+												</div>
+												<div class="info one-full column">
+													<div class="container full-width">
+														<div class="title one-full column">
+															<a href="https://www.linkedin.com/in/mar-perez-08423228" rel="external" target="_blank">Mar Perez</a>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<!-- post -->
+
+										<!-- post -->
+										<div class="post column active">
+											<div class="container full-width">
+												<div class="image one-full column">
+													<a href="https://www.linkedin.com/in/jomoll" rel="external" target="_blank">
+														<img width="640" height="400" src="https://res.cloudinary.com/vwp/image/upload/x_0,y_10,c_crop,h_530,w_530/c_thumb,w_250/v1511147201/Foto_JNMN_t92ocg.jpg" alt="">
+													</a>
+												</div>
+												<div class="info one-full column">
+													<div class="container full-width">
+														<div class="title one-full column">
+															<a href="https://www.linkedin.com/in/jomoll" rel="external" target="_blank">Jorge Moll</a>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<!-- post -->
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Team -->
+
+
+
+		<!-- contact -->
+		<section id="contact" class="section contact">
+			<div class="section-header section-cover container full-width">
+				<div class="media one-full column">
+					<div class="background image overlay-none" data-url="./assets/images/space.jpg"></div>
+				</div>
+				<div class="content one-full column">
+					<div class="container full-height h-center v-center">
+						<div class="headline one-full column">Contact</div>
+					</div>
+				</div>
+			</div>
+			<div class="section-content container v-top">
+				<div class="info one-half column">
+					<div class="container full-width">
+						<div class="headline one-full column">Reach Us</div>
+						<div class="description one-full column">Fill the form out and we will get back to you right away. You can also submit a deal on <a href="https://www.venturehealth.com/" target="_blank" style="color: #2880b2"><span>VentureHealth</span></a></div>
+						<div class="phone info-item one-full column">
+							<a href="tel:1-408-457-3700" target="_blank">(408) 457-3700</a>
+						</div>
+						<div class="email info-item one-full column">
+							<a href="mailto:contact@vhmcapital.com" target="_blank">contact@vhmcapital.com</a>
+						</div>
+						<div class="address info-item one-full column">2051 Ringwood Ave,<br>San Jose, CA, 95131</div>
+					</div>
+					<!-- <div class="cta one-full column">
+						<a href="https://www.venturehealth.com/" target="_blank" class="button light"><span>Submit a Deal on VentureHealth</span></a>
+					</div> -->
+				</div>
+				<div class="form one-half column">
+					<div class="container full-width">
+						<form>
+							<div class="form-input one-full column">
+								<label for="name">Name</label>
+								<input type="text" name="name" class="input-name input-with-border" placeholder="Mike Welley"> 
+							</div>
+							<div class="form-input one-full column">
+								<label for="email">Email</label>
+								<input type="email" name="email" class="input-email input-with-border" placeholder="john@doe.com">
+							</div>
+							<!-- <div class="form-input one-full column">
+								<label for="subject">Subject</label>
+								<input type="text" name="subject" class="input-subject input-with-border" placeholder="Need photos">
+							</div> -->
+							<div class="form-input form-input-margined one-full column">
+								<label for="message">Message</label>
+								<textarea name="message" class="input-message input-with-border" placeholder="Few words will suffice.."></textarea>
+							</div>
+							<div class="form-actions one-full column">
+								<button type="submit" class="button button-submit button-full-width">
+									<span>Send</span>
+								</button>
+							</div>
+							<div class="form-messages one-full column">
+								<div class="message message-success">
+									<span class="text-small">You message has been sent successfully!</span>
+								</div>
+								<div class="message message-error">
+									<span class="text-small">Your message could not be sent.</span>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- contact -->
+
+
+
+	</div>
+	<!-- wrapper -->
+
+	<?php include "./inc/_footer.php" ; ?>
+
+	
+
+</body>
+</html>
